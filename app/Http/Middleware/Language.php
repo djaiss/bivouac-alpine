956ffd0b4c1900c6ef102228a23c59c4114f6f1e
Regis Freyd <djaiss@users.noreply.github.com>
2023-09-15 08:17:34 -0400

    Update Language.php

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Language
 {
@@ -18,7 +19,7 @@ class Language
         $this->request = $request;
     }
 
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $this->app->setLocale(session('current_locale', config('app.locale')));
 
