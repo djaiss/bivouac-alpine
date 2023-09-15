fbef4d49e12ac06c98da2bcf4500ba41d6559f1f
Mazarin <djaiss@users.noreply.github.com>
2023-09-15 08:22:48 -0400

    chore: i18n (#3)

--- resources/views/settings/organization/index.blade.php
+++ resources/views/settings/organization/index.blade.php
@@ -7,13 +7,15 @@
         <ol class="inline-flex items-center space-x-1 md:space-x-3">
           <li class="inline-flex items-center">
             <x-link class="text-sm"
-                    href="{{ route('dashboard') }}">{{ __('Home') }}</x-link>
+                    href="{{ route('dashboard') }}"
+                    wire:navigate>{{ __('Home') }}</x-link>
           </li>
           <li>
             <div class="flex items-center">
               <x-heroicon-s-chevron-right class="mr-2 h-4 w-4 text-gray-400" />
               <x-link class="text-sm"
-                      href="{{ route('settings.index') }}">{{ __('Account settings') }}</x-link>
+                      href="{{ route('settings.index') }}"
+                      wire:navigate>{{ __('Account settings') }}</x-link>
             </div>
           </li>
           <li>
